dc93a4d2782b4f73b04bb18641d9f29feb8913da
Robert Hensing <robert@roberthensing.nl>
2024-02-23 11:27:18 +0100

    Add outPath short-circuiting tests

--- tests/default.nix
+++ tests/default.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {}, lib ? pkgs.lib }:
 
 let
   testdata = import ./testdata.nix { inherit pkgs; };
@@ -22,19 +22,40 @@ in
   subdir-9 = runner.makeTest { name = "subdir-9"; rootDir = testdata.sourceUnfiltered + "/test-tree"; subpath = "9-expected"; };
   subdir-10 = runner.makeTest { name = "subdir-10"; rootDir = testdata.sourceUnfiltered + "/test-tree"; subpath = "10-subdir-ignoring-itself"; };
 
+  # Make sure the files aren't added to the store before filtering.
+  shortcircuit = runner.makeTest {
+    name = "nested";
+    rootDir = testdata.sourceUnfilteredRecursive + "/test-tree";
+    preCheck = ''
+      # Instead of a file, create a fifo so that the filter would error out if it tries to add it to the store.
+      rm 1-simpl/1
+      mkfifo 1-simpl/1
+    '';
+  };
+
   unit-tests =
     let gitignoreNix = import ../default.nix { inherit (pkgs) lib; };
-        inherit (gitignoreNix) gitignoreFilterWith;
+        inherit (gitignoreNix) gitignoreFilterWith gitignoreSourceWith;
         example = gitignoreFilterWith { basePath = ./.; extraRules = ''
           *.foo
           !*.bar
         ''; };
+        shortcircuit = gitignoreSourceWith {
+          path = {
+            inherit (lib.cleanSource ./.) _isLibCleanSourceWith filter name;
+            outPath = throw "do not use outPath";
+            origSrc = ./.;
+          };
+        };
     in
 
     # Test that extraRules works:
     assert example ./x.foo "regular" == false;
     assert example ./x.bar "regular" == true;
     assert example ./x.qux "regular" == true;
+
+    # Make sure outPath is not used. (It's not about the store path)
+    assert lib.hasPrefix builtins.storeDir "${shortcircuit}";
 
     # End of test. (a drv to show a buildable attr when successful)
     pkgs.emptyFile or null;
