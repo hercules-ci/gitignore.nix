71f8c3a2e66302af71352c452b27164cec5d4f5f
Robert Hensing <robert@roberthensing.nl>
2024-02-28 03:18:26 +0100

    ci.nix: Drop nixos-19.03

--- nix/ci.nix
+++ nix/ci.nix
@@ -5,7 +5,6 @@ let
   ;
   in
   dimension "Nixpkgs" {
-    "nixpkgs-19_03" = sources."nixos-19.03";
     "nixpkgs-19_09" = sources."nixos-19.09";
     "nixpkgs-21_05" = sources."nixos-21.05";
   } (_key: nixpkgs:
